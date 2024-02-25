7b454f8da07525cebba2b0f02c6040e4c4d54d6a
Mariusz Najwer <najwer23@gmail.com>
2024-02-25 14:58:51 +0100

    Bench for frequency

---
+++ src/frequency/bench/frequencyArrLength_1e2.bench.js
@@ -0,0 +1,22 @@
+import { bench } from "vitest";
+import { frequencyLoopFor } from "../loopFor/js/frequencyLoopFor";
+import { frequencyLoopReduce } from "../loopReduce/js/frequencyLoopReduce";
+
+const testArr = Array.from({ length: 1e2 }, () => Math.floor(Math.random() * 40));
+
+bench(
+	"frequencyLoopFor",
+	() => {
+		frequencyLoopFor(testArr);
+	},
+	{ time: 1000 }
+);
+
+bench(
+	"frequencyLoopReduce",
+	() => {
+		frequencyLoopReduce(testArr);
+	},
+	{ time: 1000 }
+);
+
--- src/frequency/loopFor/js/frequencyLoopFor.test.js
+++ src/frequency/loopFor/js/frequencyLoopFor.test.js
@@ -3,35 +3,31 @@ import { frequencyLoopFor } from "./frequencyLoopFor";
 
 test(`frequency [1,2,3] to equal {"1": 1, "2": 1, "3": 1}`, () => {
 	expect(frequencyLoopFor([1, 2, 3])).toStrictEqual({
-		"1": 1,
-		"2": 1,
-		"3": 1,
+		1: 1,
+		2: 1,
+		3: 1,
 	});
 });
 
 test(`frequency [1,2,3,3] to equal {"1": 1, "2": 1, "3": 2}`, () => {
 	expect(frequencyLoopFor([1, 2, 3, 3])).toStrictEqual({
-		"1": 1,
-		"2": 1,
-		"3": 2,
+		1: 1,
+		2: 1,
+		3: 2,
 	});
 });
 
 test(`frequency [1,2,3,3] to equal {"a": 2, "2": 1, "3": 2}`, () => {
 	expect(frequencyLoopFor(["a", "a", 2, 3, 3])).toStrictEqual({
 		a: 2,
-		"2": 1,
-		"3": 2,
+		2: 1,
+		3: 2,
 	});
 });
 
 test(`frequency [-1,-1,3,3] to equal {"-1": 2, "3": 2}`, () => {
 	expect(frequencyLoopFor(["-1", -1, 3, 3])).toStrictEqual({
 		"-1": 2,
-		"3": 2,
+		3: 2,
 	});
 });
-
-
-
-
