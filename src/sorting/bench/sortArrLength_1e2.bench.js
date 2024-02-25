15c66543d51926de7e44e3fcf758d20cf419f4d9
Mariusz Najwer <najwer23@gmail.com>
2024-02-25 21:47:27 +0100

    Bench sort

---
+++ src/sorting/bench/sortArrLength_1e2.bench.js
@@ -0,0 +1,23 @@
+import { bench } from "vitest";
+import { quickSort } from "../quickSort/js/quickSort";
+
+const testArr = Array.from({ length: 1e2 }, () =>
+	Math.floor(Math.random() * 1000)
+);
+const testArr2 = structuredClone(testArr);
+
+bench(
+	"quickSort",
+	() => {
+		quickSort(testArr);
+	},
+	{ time: 1000 }
+);
+
+bench(
+	"sort()",
+	() => {
+		testArr2.sort();
+	},
+	{ time: 1000 }
+);
